ef25fb81b1276ea92b0af1d061939ac133f17cee
Aziz Anwar <azisanw19@gmail.com>
2023-04-29 13:47:15 +0700

    spring init

---
+++ src/main/kotlin/com/canwar/spring/restful/api/SpringRestfulApiApplication.kt
@@ -0,0 +1,11 @@
+package com.canwar.spring.restful.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRestfulApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringRestfulApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/canwar/spring/restful/api/SpringRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.canwar.spring.restful.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
